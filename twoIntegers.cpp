74f36d46bccaf1bd550bd56844f32e431b4314f5
Osman <osman.onsoy6640@gmail.com>
2025-01-08 15:35:54 +0300

    Add files via upload

---
+++ twoIntegers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number1;
+    int number2;
+
+    int total = 0;
+
+
+    total = number1 + number2;
+
+    printf("Toplam deger: ")
+
+
+    return 0;
+}
