6c4b00e5b9947c8a370786349078b19a60980e3e
Osman <osman.onsoy6640@gmail.com>
2025-01-08 15:34:49 +0300

    Add files via upload

---
+++ aritmetrik.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int number1;
+    int number2;
+
+    cout << "Enter two integer numbers: ";
+    cin >> number1 >> number2;
+
+    cout <<"Total is " << number1 + number2 << endl;
+    cout <<"Subtraction is " << number1 - number2 << endl;
+    cout <<"Multiplication is " << number1 * number2 << endl;
+    cout <<"Division is " << number1 / number2 << endl;
+
+
+    return 0;
+}
